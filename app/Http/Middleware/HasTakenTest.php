dc16db97e1782893fb26efec6a4e86c952e6a680
AlbaMarm <alba03943@gmail.com>
2025-05-25 11:52:21 +0200

    feat: middleware para admin y para controlar si se ha hecho el test o no

---
+++ app/Http/Middleware/HasTakenTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class HasTakenTest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+        
+        /** @var \App\Models\User $user */
+        $user = Auth::user()->load('test'); 
+
+        if (!$user->test) {
+            return redirect()->route('personality.test');
+        }
+
+        return $next($request);
+    }
+}
