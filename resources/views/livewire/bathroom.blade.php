778edbae11047965a71c73d554d3713b1e89a916
AlbaMarm <alba03943@gmail.com>
2025-05-29 13:30:35 +0200

    feat: stats como componente y monedas actualizables

--- resources/views/livewire/bathroom.blade.php
+++ resources/views/livewire/bathroom.blade.php
@@ -5,14 +5,14 @@
     <div class="absolute top-6 left-4 md:left-6 flex flex-col space-y-6 z-30">
         {{-- Monedas --}}
         <div class="flex items-center space-x-4 bg-white bg-opacity-90 border border-gray-200 rounded-full px-6 py-4 shadow-2xl">
-            <img src="{{ asset('images/icons/money.png') }}" class="h-10 w-10 hover-bounce" alt="Coins">
-            <span id="coin-value" class="text-gray-800 text-3xl font-extrabold">{{ $coins }}</span>
+            <img src="{{ asset('images/icons/money.png') }}" class="h-14 w-14 hover-bounce" alt="Coins">
+            <span id="coin-value" class="text-gray-800 text-5xl font-extrabold">{{ $coins }}</span>
         </div>
 
         {{-- Felicidad --}}
         <div class="flex items-center space-x-4 bg-black bg-opacity-70 border border-white rounded-full px-6 py-4 shadow-2xl">
-            <img src="{{ asset('images/icons/happy.png') }}" class="h-10 w-10 hover-bounce" alt="Happiness">
-            <span id="happiness-value" class="text-white text-3xl font-extrabold">{{ $pet->happiness }}</span>
+            <img src="{{ asset('images/icons/happy.png') }}" class="h-14 w-14 hover-bounce" alt="Happiness">
+            <span id="happiness-value" class="text-white text-5xl font-extrabold">{{ $pet->happiness }}</span>
         </div>
     </div>
 
@@ -42,23 +42,7 @@
     </div>
 
     {{-- PANEL DE ESTADÍSTICAS --}}
-    <aside
-        class="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-full max-w-sm md:absolute md:bottom-auto md:top-6 md:right-6 md:left-auto md:translate-x-0 md:w-80 p-4 rounded-2xl shadow-xl z-20"
-        style="background-image: url('{{ asset('images/bathtexture.png') }}'); background-size: cover; background-repeat: no-repeat;">
-        <h1 class="text-xl font-bold text-black mb-4">STATS</h1>
-
-        @foreach($statsList as $s)
-        <div class="mb-6">
-            <div class="flex items-center space-x-3 mb-2 text-black font-bold text-lg md:text-2xl">
-                <img src="{{ asset('images/icons/'.$s['icon']) }}" class="h-8 w-8 md:h-10 md:w-10 hover-bounce" alt="{{ $s['label'] }}">
-                <span>{{ $s['label'] }}</span>
-            </div>
-            <div class="w-full h-6 md:h-8 bg-white rounded-xl overflow-hidden shadow-inner border-2 border-gray-300">
-                <div class="h-full transition-all duration-300" style="width: {{ $s['value'] }}%; background-color: {{ $s['color'] }};"></div>
-            </div>
-        </div>
-        @endforeach
-    </aside>
+    <x-statspanel :statsList="$statsList" background="images/bathtexture.png" />
 
     {{-- BOTÓN SHOWER --}}
     <div class="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-30">
