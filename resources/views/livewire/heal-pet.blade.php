a5b1bf8e6f3bcb9743d85d8f7f9617d323b0c2af
AlbaMarm <alba03943@gmail.com>
2025-06-04 13:48:30 +0200

    feat: traducido al inglés

--- resources/views/livewire/heal-pet.blade.php
+++ resources/views/livewire/heal-pet.blade.php
@@ -1,27 +1,28 @@
 <div>
     <x-button wire:click="$set('openModal', true)">
-        <i class="fa-solid fa-hospital mr-2"></i> Curar Mascota
+        <i class="fa-solid fa-hospital mr-2"></i> Heal Pet
+        <span class="text-xs font-bold text-green-200">[100%]</span>
     </x-button>
 
     <x-dialog-modal wire:model="openModal">
         <x-slot name="title">
-            <span class="text-red-600 font-bold">Curar a la mascota</span>
+            <span class="text-red-600 font-bold">Heal Pet</span>
         </x-slot>
 
         <x-slot name="content">
-            <p>Esto restaurará todos los estados a <strong>100</strong>.</p>
-            <p class="font-bold text-red-300 mt-2">¡¡¡ Usar solo en caso extremo !!!</p>
+            <p>This will restore all states to <strong>100</strong>.</p>
+            <p class="font-bold text-red-300 mt-2">Use only in extreme cases !!!</p>
         </x-slot>
 
         <x-slot name="footer">
             <div class="flex justify-end space-x-4">
                 <button wire:click="curar"
                     class="inline-flex items-center px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
-                    <i class="fas fa-heart-pulse mr-2"></i> Aceptar
+                    <i class="fas fa-heart-pulse mr-2"></i> Accept
                 </button>
                 <button wire:click="cerrar"
                     class="inline-flex items-center px-4 py-2 bg-gray-200 text-red-800 rounded hover:bg-gray-300">
-                    <i class="fas fa-ban mr-2"></i> Cancelar
+                    <i class="fas fa-ban mr-2"></i> Cancel
                 </button>
             </div>
         </x-slot>
