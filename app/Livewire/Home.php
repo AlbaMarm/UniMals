92c54e9788bddac0a3d9634dedd5a32e3899c416
AlbaMarm <alba03943@gmail.com>
2025-06-05 08:21:40 +0200

    feat: refactorizado codigo

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -9,7 +9,6 @@ use Livewire\Component;
 
 class Home extends Component
 {
-    //refresca la pagina para que se actualicen los datos
     protected $listeners = ['refreshComponent' => '$refresh'];
     public $pet;
     public $lastAccessory;
